394f763c8f72afdd2104171d4bb8e4b293f58df8
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-10 18:31:47 +0000

    Atividades

---
+++ fup/resto/go/draft.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main() {
+    var bonbons, crianças int
+    fmt.Scan ()
+    fmt.Println("qxcode")
+}
---
+++ sandbox/00_rascunhos/primeiro codigo/draft.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    var num1, num2 float64 //variavél
+   
+    fmt.Scan(&num2, &num2) //lendo e escrevendo EM num1
+    fmt.Println("olá mundo" , num1, num2)
+}
