36a37dda40bb3839c7d4d7b88cbbd6edc68cc1f0
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-17 18:15:19 +0000

    Rascunhos

---
+++ fup/segundos/go/draft.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func main() {
+    var segund float64
+    fmt.Scan(&segundo )
+
+    fmt.Println("")
+}
--- sandbox/00_rascunhos/Deisi/draft.go
+++ sandbox/00_rascunhos/Deisi/draft.go
@@ -1,10 +1,39 @@
+// package main
+// import "fmt"
+// func main() {
+//     var dia string = "seg"
+//     if dia == "seg" || 
+//     fmt.Println("Quer casar comigo? digite 1")
+// }
+
+
+
 package main
-import "fmt"
-func main() {
-    var sim int
-    fmt.Println("Quer casar comigo? digite 1")
-    fmt.Scan(&sim)
-    fmt.Println("Que alegria, vamos marcar o dia")
-  
-}
+// import "fmt"
+// func main() {
+
+// idade := 17
+// if idade < 12 {
+//     fmt.Println("crianca")
+// }else if idade < 18 {
+//     fmt.Println("teen")
+// }else if idade < 65 {
+//     fmt. Println("idoso")
+// }
+
+// jog1 := "pedra"
+// jog2 := "papel"
+// "jog1", jog2
+// if jog1 == jog2 {
+//     fmt.Printçn("empate")
+// } else if (jog1 == "pedra" && jog2 == "tesoura") ||
+//           (jog1 == "papel" && jog2 == "pedra") ||
+
+
+
+jog1 := 0
+jog2 := 1
+jog3 := 0
+if jog1 == jog2 && jog2 == jog3
+
 
