1b6bf3c982c7723119e05f487d0d396c44222430
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-17 00:26:26 +0000

    Atividades

--- fup/resto/go/draft.go
+++ fup/resto/go/draft.go
@@ -6,4 +6,5 @@ func main() {
 	var bombons, criancas int
 	fmt.Scan(&bombons, &criancas)
 	fmt.Println(bombons/criancas, bombons % criancas)
+	
 }
--- fup/sobrou/go/draft.go
+++ fup/sobrou/go/draft.go
@@ -1,5 +1,14 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var a, b, c, w, y, z, h float64
+     fmt.Scan(&a)
+     fmt.Scan(&b)
+     fmt.Scan(&c)
+     fmt.Scan(&w)
+     fmt.Scan(&y)
+     fmt.Scan(&z)
+     fmt.Scan(&h)
+    fmt.Printf("%.2f\n", ((a*w) + (b*y) + (c*z) - h)*(-1))
+
 }
