3dc0631e96c9eb4b06658bd7e9008a09c39a8662
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-17 01:01:10 +0000

    Teste 01

---
+++ sandbox/00_rascunhos/Deisi/draft.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+    var sim int
+    fmt.Println("Quer casar comigo? digite 1")
+    fmt.Scan(&sim)
+    fmt.Println("Que alegria, vamos marcar o dia")
+  
+}
+
