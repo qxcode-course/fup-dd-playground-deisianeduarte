f4af94bfe4fd9b4e6d45f62103b9a2fc3a9050f2
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-22 15:27:42 +0000

    Atividades

--- fup/pintando/go/draft.go
+++ fup/pintando/go/draft.go
@@ -1,5 +1,11 @@
 package main
 import "fmt"
+import "math"
 func main() {
-    fmt.Println("qxcode")
+    var a, b, c, p, s, at float64
+    fmt.Scan(&a, &b, &c)
+    p = (a + b + c) / 2
+    s = p * (p - a) * (p - b) * (p - c)
+    at = math.Sqrt(s)
+    fmt.Printf("%.2f\n", at)
 }
