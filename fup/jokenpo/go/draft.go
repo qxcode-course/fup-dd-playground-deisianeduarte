e5c6e9f6b5d30d360d4dcec254da3649783ca0d9
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-22 16:29:45 +0000

    Atividade

---
+++ fup/jokenpo/go/draft.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main() {
+
+    var jog1, jog2 string
+    fmt.Scan(&jog1)
+    fmt.Scan(&jog2)
+    if jog1 == jog2 {
+    fmt.Println("empate")
+    } else if (jog1 == "R" && jog2 == "S") ||
+    (jog1 == "P" && jog2 == "R") ||
+    (jog1 == "S" && jog2 == "P") { 
+        fmt.Println("jog1") 
+    } else {
+        fmt.Println("jog2")
+    }
+}
+//R (pedra), P (papel), e S (tesoura)
