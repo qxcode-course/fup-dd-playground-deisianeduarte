ab274ed22b25f97c31c533910ac7de047bd2b5e3
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-23 18:14:49 +0000

    Atividades

--- fup/calc/go/draft.go
+++ fup/calc/go/draft.go
@@ -1,6 +1,36 @@
 package main
 import "fmt"
 func main() {
+    var a, b int
+    var operador string
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+    fmt.Scan(&operador)
+
+    resultado := 0
+    valid := true
+
+    switch operador{
+    case "+":
+    resultado = a + b 
+    case "-":
+    resultado = a - b
+    case "*": 
+    resultado = a * b 
+    case "/":
+        if b == 0 {
+            fmt.Println("ERRO, divisao por zero") 
+            valid = false
+        } else {
+            resultado = a / b
+        }
     
-    fmt.Println("Hello, World!")
-}
+         default:
+		fmt.Println("Operação inválida!")
+		valid = false
+         }
+
+	if valid {
+		fmt.Println(resultado)
+	}
+    }
--- sandbox/00_rascunhos/Deisi/draft.go
+++ sandbox/00_rascunhos/Deisi/draft.go
@@ -23,20 +23,72 @@
 
 
 
+// package main
+// import "fmt"
+// func main() {
+
+// jog1 := 0
+// jog2 := 1
+// jog3 := 0
+
+// 	if jog1 == jog2 && jog2 == jog3{
+// 		fmt.Println("empate")
+// 	} else if jog2 == jog3 {
+// 		fmt.Println("jog2")
+// 	} else {
+// 		fmt.Println("jog3")
+// 	}
+
+// }
+
+package main
+import "fmt"
+func main() {
+    var a, b, d int 
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+    d = a - b
+    if d <0 {
+    fmt.Println(d * (-1))
+    } else {
+    fmt.Println(d)
+    }
+}
+
+package main
+import "fmt"
+import "math"
+func main() {
+    var x1, y1, x2, y2, d float64
+    fmt.Scan(&x1, &y1, &x2, &y2)
+    d = math.Sqrt(math.Pow(x2 - x1, 2) + math.Pow(y2 - y1, 2))
+    fmt.Printf("%.2f\n", d)
+}
+
+package main
+import "fmt"
+func main() {
+    var tc, tf float64
+    fmt.Scan(&tc)
+    tf = 1.8 * tc + 32
+    fmt.Printf("%.6f\n", tf)
+}
+
 package main
 import "fmt"
 func main() {
 
-jog1 := 0
-jog2 := 1
-jog3 := 0
-
-	if jog1 == jog2 && jog2 == jog3{
-		fmt.Println("empate")
-	} else if jog2 == jog3 {
-		fmt.Println("jog2")
-	} else {
-		fmt.Println("jog3")
-	}
-
+    var jog1, jog2 string
+    fmt.Scan(&jog1)
+    fmt.Scan(&jog2)
+    if jog1 == jog2 {
+    fmt.Println("empate")
+    } else if (jog1 == "R" && jog2 == "S") ||
+    (jog1 == "P" && jog2 == "R") ||
+    (jog1 == "S" && jog2 == "P") { 
+        fmt.Println("jog1") 
+    } else {
+        fmt.Println("jog2")
+    }
 }
+//R (pedra), P (papel), e S (tesoura)
