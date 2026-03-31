0c1556c191c619de0a37f89fb4e6432c0044e507
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-31 00:44:12 +0000

    Atividades

--- fup/impar/go/draft.go
+++ fup/impar/go/draft.go
@@ -4,25 +4,25 @@ func main() {
     var p, d1, d2 int64
     fmt.Scan(&p)
     fmt.Scan(&d1)
-    fmt.Scan(d2)
+    fmt.Scan(&d2)
     soma := d1 + d2
+    
     // se a soma for par
-    if soma%2 == 0 {
-    if p == 0 {
-        fmt.Println(0)
-        }//a Alice ganha
-    } else{
-        //se a soma for ímpar
-        fmt.Println(1)
-        //o Bob ganha 
 
-    } else {
-        if p == 0 {
-            fmt.Println(1)
-        } else {
-            fmt.Println(0)
+    if soma%2 == 0 && p == 0 {
+        fmt.Println(0)
         }
-    }
-    
-    
+        
+        //a Alice ganha
+      
+    if soma%2 == 0 && p == 1 {
+        fmt.Println(1)
+
+    } 
+    if soma%2 != 0 && p == 0 {
+        fmt.Println(1)
+        }
+    if soma%2 != 0 && p == 1 {
+        fmt.Println(0)
+        }
 }
