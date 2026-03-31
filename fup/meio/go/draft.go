89d2f15a2d338d70e3f28bc2ec7897f95222ff7a
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-31 01:45:06 +0000

    Atividades

--- fup/meio/go/draft.go
+++ fup/meio/go/draft.go
@@ -1,5 +1,17 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var a, b, c int64
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+    fmt.Scan(&c)
+    if a > b && a < c || a < b && a > c{
+    fmt.Println(a)
+    }
+    if b > a && b < c || b < a && b > c {
+        fmt.Println(b)
+    } 
+    if c > a && c < b || c < a && c > b {
+        fmt.Println(c)
+    }
 }
