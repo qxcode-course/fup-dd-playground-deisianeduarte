86148fbeaeb2007778cb4935266f53da3491e6b7
deisianeduarte <deisiane.duarte@alu.ufc.br>
2026-03-31 18:18:35 +0000

    Jogos

--- fup/pr_jokenpo/go/draft.go
+++ fup/pr_jokenpo/go/draft.go
@@ -1,5 +1,42 @@
 package main
 import "fmt"
+
 func main() {
-    fmt.Println("Hello, World!")
+    const PEDRA int = 0
+    const PAPEL int = 1
+    const TESOURA int = 2 
+    
+    var jog1, jog2  int
+    var vit1, vit2
+
+    fmt.Println("jog1:digite 0(pedra), 1(papel), 2(tesoura)")
+    fmt.Scan(&jog1)
+    fmt.Println("jog1:digite 0(pedra), 1(papel), 2(tesoura)")
+     fmt.Scan(&jog2)
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// variavel constante valores que nao mudam (palavras com letras maiusculas)
